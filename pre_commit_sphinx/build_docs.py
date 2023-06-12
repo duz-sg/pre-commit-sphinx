85f7c1c31fbfda24522c6d958637178fc76b926f
Du, Zheng <zheng.du@changehealthcare.com>
2023-06-12 12:03:43 -0400

    run sphinx-apidoc each time

--- pre_commit_sphinx/build_docs.py
+++ pre_commit_sphinx/build_docs.py
@@ -5,8 +5,7 @@ from typing import Sequence
 
 
 def requires_build(filenames: Sequence[str], always_build: bool) -> bool:
-    """ Uses filenames list to check if anything has changed in the documentation folder.
-    """
+    """Uses filenames list to check if anything has changed in the documentation folder."""
     if not always_build:
         # Always return true for now (we rebuild with breathe/exhale so source code changes also require doc build)
         # In future allow rebuild e.g. only if files in the docs directory change
@@ -15,11 +14,12 @@ def requires_build(filenames: Sequence[str], always_build: bool) -> bool:
 
 
 def build(cache_dir: str, html_dir: str, src_dir: str):
-    """ Invokes sphinx-build to build the docs
-    """
+    """Invokes sphinx-build to build the docs"""
 
     # Run Sphinx to build the documentation
-    ret = os.system(f'sphinx-build -b html -d {cache_dir} {src_dir} {html_dir}; make clean; make html')
+    ret = os.system(
+        f"sphinx-apidoc -o {src_dir} {src_dir} -f; sphinx-build -b html -d {cache_dir} {src_dir} {html_dir}; make clean; make html"
+    )
 
     # It's very weird that pre-commit marks this as 'PASSED' if I return an error code 512...! Workaround:
     return 0 if ret == 0 else 1
@@ -28,25 +28,34 @@ def build(cache_dir: str, html_dir: str, src_dir: str):
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'filenames', nargs='*',
-        help='Filenames pre-commit believes are changed',
+        "filenames",
+        nargs="*",
+        help="Filenames pre-commit believes are changed",
     )
     parser.add_argument(
-        '--always-build', type=bool, default=True,
-        help='Always rebuild, even if no doc files have changed (useful if using \
-             breathe/exhale to extract docstrings from code)',
+        "--always-build",
+        type=bool,
+        default=True,
+        help="Always rebuild, even if no doc files have changed (useful if using \
+             breathe/exhale to extract docstrings from code)",
     )
     parser.add_argument(
-        '--cache-dir', type=str, default='docs/doctrees',
-        help='Directory to cache doctrees for fast rebuild where there are few changes',
+        "--cache-dir",
+        type=str,
+        default="docs/doctrees",
+        help="Directory to cache doctrees for fast rebuild where there are few changes",
     )
     parser.add_argument(
-        '--html-dir', type=str, default='docs/html',
-        help='Directory to output the build html',
+        "--html-dir",
+        type=str,
+        default="docs/html",
+        help="Directory to output the build html",
     )
     parser.add_argument(
-        '--source-dir', type=str, default='docs/source',
-        help='Directory containing documentation sources (where the conf.py file exists)',
+        "--source-dir",
+        type=str,
+        default="docs/source",
+        help="Directory containing documentation sources (where the conf.py file exists)",
     )
 
     args = parser.parse_args(argv)
@@ -56,5 +65,5 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
