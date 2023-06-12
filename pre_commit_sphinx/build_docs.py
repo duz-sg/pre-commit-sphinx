2f882b32aa7c4c29c8b7aef6dab890baa54cf807
Du, Zheng <zheng.du@changehealthcare.com>
2023-06-12 12:13:49 -0400

    Remove sphinx-build

--- pre_commit_sphinx/build_docs.py
+++ pre_commit_sphinx/build_docs.py
@@ -18,7 +18,7 @@ def build(cache_dir: str, html_dir: str, src_dir: str):
 
     # Run Sphinx to build the documentation
     ret = os.system(
-        f"sphinx-apidoc -o {src_dir} {src_dir} -f; sphinx-build -b html -d {cache_dir} {src_dir} {html_dir}; make clean; make html"
+        f"sphinx-apidoc -o {src_dir} {src_dir} -f; make clean; make html"
     )
 
     # It's very weird that pre-commit marks this as 'PASSED' if I return an error code 512...! Workaround:
